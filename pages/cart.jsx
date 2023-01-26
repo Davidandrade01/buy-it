c6ed0f31abd4f3a1297ec8a060a926d7fea9af39
Davidandrade01 <david.andrade01@live.com>
2023-01-26 16:35:08 +0000

    update CartPage

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,3 +1,19 @@
+import React, { useState } from 'react'
+import { getItem } from '../services/LocalStorageproducts'
+
+export default function CartPage() {
+    const [data,setData]=useState(getItem('basket')||[])
+    
+  return (
+    <div>
+      <div>Cart</div>
+      <div>
+        {data.map((e)=><li key={e.id}>{e.id}</li>)}
+      </div>
+        
+    </div>
+  ) 
+}
 
 
 
