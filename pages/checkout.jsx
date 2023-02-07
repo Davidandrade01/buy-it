eedcbca2d8fb80dbdfce2ea93f37f9526dfdca64
Davidandrade01 <david.andrade01@live.com>
2023-02-07 12:14:35 +0000

    update apiCp

--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -7,20 +7,25 @@ import { useForm } from 'react-hook-form'
 export default function ShippingScreen  () {
   const {register,setFocus,handleSubmit,setValue}=useForm()
 
- 
-
+  
+  const onSubmit=(e)=>{
+    console.log(e)
+  }
   
   
     const onblurzip=async (e)=>{
       e.preventDefault()
       const zip=e.target.value.replace(/[^0-9]/g,'')
-      
-      const res = await fetch(`https://json.geoapi.pt/cp/${zip}`)
+      const apiId=  "ptapi63e0e78054fb56.05582833"
+      const res = await fetch(` https://api.duminio.com/ptcp/v2/${apiId}/${zip}`)
       .then(  res => res.json()).then( data => {
-       console.log(data)
-       setValue('Street', data.ruas);
-       setValue('city', data.Concelho)
-       setValue("district",data.Distrito)
+       //console.log(data)
+       
+       setValue('Street', data[0].Morada);
+       setValue("city",data[0].Concelho)
+       setValue("neighborhood",data[0].Freguesia)
+       setValue("district",data[0].Distrito)
+       setFocus("number")
       
        
       
@@ -31,14 +36,14 @@ export default function ShippingScreen  () {
     <>
     
       <CheckoutWizard activeStep={1}/>
-      <form  className="mx-auto max-w-screen-md"  >
+      <form onSubmit={handleSubmit(onSubmit)} className="mx-auto max-w-screen-md"  >
       
  <h1 className="mb-4 text-xl"><b>Shipping</b></h1>
 
  <label className='m-80'><input type="checkbox"  /> Use my adress</label>
 
 <div className="relative z-0 w-full mb-6 group">
-      <input type="text" id="fullname" className="floating-input peer" placeholder=" " 
+      <input type="text" id="fullname" className="floating-input peer" placeholder=" " required 
        />   
       <label htmlFor="fullname" className="floating-label">
       
@@ -46,7 +51,8 @@ export default function ShippingScreen  () {
 </div>
 
   <div className="relative z-0 w-full mb-6 group">
-        <input onBlur={onblurzip}  type="text" id="Zipcode" className="floating-input peer" placeholder=" " 
+        <input onBlur={onblurzip}  type="text" id="Zipcode" className="floating-input peer"
+         placeholder=" "  required 
          />
         <label htmlFor="Zip code" className="floating-label">
   
@@ -54,7 +60,8 @@ export default function ShippingScreen  () {
   </div>
   
 <div className="relative z-0 w-full mb-6 group">
-      <input {...register("Street" )}  type="text" id="street" className="floating-input peer" placeholder=" " 
+      <input {...register("Street" )}  type="text" id="street" className="floating-input peer"
+       placeholder=" " required 
        />   
       <label  htmlFor="street" className="floating-label">
       
@@ -62,7 +69,8 @@ export default function ShippingScreen  () {
 </div>
 
 <div className="relative z-0 w-44 mb-6 group">
-      <input {...register("number")}  type="text" id="number" className="floating-input peer" placeholder=" " 
+      <input {...register("number")}  type="text" id="number" className="floating-input peer"
+       placeholder=" " required  
        />   
       <label htmlFor="number" className="floating-label">
       
@@ -79,7 +87,8 @@ export default function ShippingScreen  () {
 
 <div className='flex justify-between'>
   <div className="relative z-0 w-full mb-6 group">
-        <input {...register("city")} type="text" id="city" className="floating-input peer" placeholder=" " 
+        <input {...register("city")} type="text" id="city" className="floating-input peer" 
+        placeholder=" " required 
          />
         <label htmlFor="city" className="floating-label">
   
@@ -87,7 +96,8 @@ export default function ShippingScreen  () {
   </div>
   
   <div className="relative z-0 w-full mb-6 group">
-        <input {...register("neighborhood")} type="text" id="neighborhood" className="floating-input peer" placeholder=" " 
+        <input {...register("neighborhood")} type="text" id="neighborhood" className="floating-input peer"
+         placeholder=" " required 
          />
         <label htmlFor="neighborhood" className="floating-label">
   
@@ -97,7 +107,8 @@ export default function ShippingScreen  () {
 
 <div className='flex justify-between'>
   <div className="relative z-0 w-full mb-6 group">
-        <input {...register("district")} type="text" id="county" className="floating-input peer" placeholder=" " 
+        <input {...register("district")} type="text" id="county" className="floating-input peer" 
+        placeholder=" " required 
          />
         <label htmlFor="district " className="floating-label">
   
@@ -105,14 +116,15 @@ export default function ShippingScreen  () {
   </div>
   
   <div className="relative z-0 w-full mb-6 group">
-        <input   type="text" id="city" className="floating-input peer" placeholder=" " 
+        <input   type="text" id="country" className="floating-input peer"
+         placeholder=" " required 
          />
         <label htmlFor="country" className="floating-label">
   
        Country *</label>
   </div>
 </div>
-  <button  type='submit' className=' mt-10 w-3/4 primary-button'>Next</button> 
+  <button  type='submit' className=' mt-08 w-3/4 primary-button'>Next</button> 
 </form>
     
     
