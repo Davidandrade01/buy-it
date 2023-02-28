ab917d14508ab4a2f6e9106cfbefcf861c49e576
Davidandrade01 <david.andrade01@live.com>
2023-02-28 12:48:32 +0000

    cards update

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import {AiFillStar} from 'react-icons/ai'
 import Link from 'next/link'
 import { useContext } from 'react'
@@ -11,30 +11,43 @@ import { useState } from 'react'
 
 
 export default function Cards({product}) {
-const[colorfavorites, setColorFavorites]=useState("btn-favorites")
-  const favoritesfunc=()=>{
-    setColorFavorites("btn-favorites2")
-  }
- 
+
+ const[favcolor,setFavcolor]=useState(false)
+
+
+
+ const changefavcolor=()=>{
+  setFavcolor(!favcolor)
+ }
+
   const {productCart, addToCart}=useContext(CartContext)
   const discount=(((product.discountPercentage)/100) *(product.price)).toFixed(2)
   const Valuewithdiscount=((product.price)-discount)
+
+
  
   return (
     
     <div style={{width:"324px",height:"451px", margin:"12px"}}>
-    <div className='absolute top-2 right-2'><MdOutlineFavoriteBorder size={24}/></div>
-      <Link href={`/product/${product.id}`}>
+ 
+      
       
         <div style={{width:"324px", height:"301px", border:"1px solid black",}}
         className='flex justify-center items-center mb-2 hover:opacity-80 relative'>
-        
-          <Image src={  product.image   } height={200} width={200}  alt={product.title} className="max-w-fit max-h-full " />
+             <MdOutlineFavoriteBorder   onClick={changefavcolor} size={24} style={{fill :favcolor ? "#D51451":"#000000"}}
+      className='absolute top-2 right-2'  
+    />
+          <Link href={`/product/${product.id}`}>
+          <div style={{width:"300px", height:"300px"}}>
+            <Image src={  product.image   } height={200} width={200}  alt={product.title} className="max-w-fit max-h-full " />
+          </div>
+          </Link>
         </div>
-      </Link>
+      
       <div className='text-left'>
           <p style={{fontSize:"12px", marginBotton:"8px"}}>{product.category}</p>
           <p style={{fontSize:"16px", marginBotton:"8px"}} className='limit-ch'>{product.title}</p>
+          
           
           {product.discountPercentage > 0  &&
           <div>
@@ -55,7 +68,8 @@ const[colorfavorites, setColorFavorites]=useState("btn-favorites")
           
           
       </div>
-      <button  style={{border:"1px solid black", marginTop:"8px"}}
+      <button 
+         style={{border:"1px solid black", marginTop:"8px"}}
          className='text-button py-2 px-4  hover:bg-black hover:text-white flex flex-initial' >
             <b>ADD TO CART</b>  
         </button>
